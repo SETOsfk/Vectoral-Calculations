98eeee7d38d2ab57fd3ebf21a0752345b6361ca7
Sertan <sertansafak@posta.mu.edu.tr>
2022-10-18 16:29:30 +0300

    VectoralCalculations

--- vektörHesabı/src/vektörHesabı/Main1.java
+++ vektörHesabı/src/vektörHesabı/Main1.java
@@ -8,70 +8,70 @@ int[] vekt1=new int[3];
 int[] vekt2=new int[3];
 int devam=1;
 while(devam==1) {
-	System.out.println("Vektör girmek için 1'e basýnýz");
-	System.out.println("Çýkýþ yapmak için 0'a basýnýz.");
+	System.out.println("Please press 1 for input a new vector");
+	System.out.println("Please press 0 for quit.");
 	int devammý=k.nextInt();
 	if(devammý==0) {
-		System.out.println("Kullandýðýnýz için teþekkürler.");
+		System.out.println("Thank you very much.");
 		k.close();
 		break;}
 	if(devammý==1) {
 for(int i=0;i<vekt1.length;i++) {
-	System.out.println("1. vektörün X,Y,Z deðerlerini sýrasýyla giriniz.");
+	System.out.println("Please add  X,Y,Z variables of first vector");
 	vekt1[i]=k.nextInt();
 	
 }
 for(int i=0;i<vekt2.length;i++) {
-	System.out.println("2. vektörün X,Y,Z deðerlerini sýrasýyla giriniz.");
+	System.out.println("Please add  X,Y,Z variables of second vector");
 	vekt2[i]=k.nextInt();}
-System.out.println("Ýki vektör arasý uzaklýk hesabý için 1");
-System.out.println("Vektörel çarpým için 2");
-System.out.println("Büyüklüklerini hesaplamak için 3");
-System.out.println("Skaler çarpým için 4");
-System.out.println("Üçgenin alaný için 5");
-int tercih=k.nextInt();
+System.out.println("Please press 1 for Vectoral Distance. ");
+System.out.println("Please press 2 for Cross Product");
+System.out.println("Please press 3 for Vector Quantity");
+System.out.println("Please press 4 for Scalar Multiplication");
+System.out.println("Please press 5 for Area of a Triangle and Area of a parallelogram");
+int  option=k.nextInt();
 
 //VEKTÖREL UZUNLUK
 
 int[] vektU=new int[3];
 int[] vektU2=new int[3];
-if(tercih==1) {
+if(option==1) {
 	for(int i=0;i<vekt1.length;i++) {
 vektU[i]= (vekt2[i]-vekt1[i]);}
-System.out.println("Vektörün uzunluk deðeri <"+vektU[0]+","+vektU[1]+","+vektU[2]+"> olur.");}
+System.out.println("Vectoral Distance is <"+vektU[0]+","+vektU[1]+","+vektU[2]+">");}
 
 //VEKTÖREL ÇARPIM
 
 
 int[] vc=new int[3];
-if(tercih==2) {	
+if(option==2) {	
 vc[0]=(vekt1[1]*vekt2[2])-(vekt2[1]*vekt1[2]);	
 vc[1]=(vekt1[0]*vekt2[2])-(vekt2[0]*vekt1[2]);
 vc[2]=(vekt1[0]*vekt2[1])-(vekt2[0]*vekt1[1]);
-System.out.println("Vektörün çarpým deðeri <"+vc[0]+","+vc[1]+","+vc[2]+"> olur.");}
+System.out.println("Cross Product is <"+vc[0]+","+vc[1]+","+vc[2]+">");}
 
 //VEKTÖREL BÜYÜKLÜK
 
 double vc1b;
 double vc2b;
-if(tercih==3) {
+if(option==3) {
 for(int i=0;i<vekt1.length;i++) {
 
 vc1b=Math.sqrt(Math.pow(vekt1[0],2)+Math.pow(vekt1[1],2)+Math.pow(vekt1[2],2));	
 vc2b=Math.sqrt(Math.pow(vekt2[0],2)+Math.pow(vekt2[1],2)+Math.pow(vekt2[2],2));	
-System.out.println("1. vektörün büyüklüðü ="+vc1b);
-System.out.println("2. vektörün büyüklüðü ="+vc2b);}}
+System.out.println("Vector Quantity of first vector ="+vc1b);
+System.out.println("Vector Quantity of second vector ="+vc2b);}}
 
 // SKALER ÇARPIM
 
 int[] skaler=new int[3];
-if(tercih==4) {
+if(option==4) {
 	for(int i=0;i<vekt1.length;i++) {
 	skaler[i]=vekt1[i]*vekt2[i];}
-	System.out.println("Skaler çarpým <"+skaler[0]+","+skaler[1]+","+skaler[2]+">");
+	System.out.println("Scalar Multiplication <"+skaler[0]+","+skaler[1]+","+skaler[2]+">");
 }
-if(tercih==5) {
-	System.out.println("Üçgen alan hesabý için 3. vektörü giriniz.");
+if(option==5) {
+	System.out.println("Please add third vector...");
 	int[] vekt3=new int[3];
 	for(int i=0;i<vekt3.length;i++) {
 	vekt3[i]=k.nextInt();	
@@ -88,8 +88,8 @@ vektU2[i]= (vekt3[i]-vekt1[i]);
 		 double vcb=Math.sqrt(Math.pow(vc[0],2)+Math.pow(vc[1],2)+Math.pow(vc[2],2));	
 		double  ua=vcb/2;
 		double  uap=vcb;
-		 System.out.println("Üçgenin alaný ="+ua);
-		 System.out.println("Paralel kenarýn alaný ="+uap);
+		 System.out.println("Area of Triangle ="+ua);
+		 System.out.println("Area of a parallelogram ="+uap);
 		}
 }
 	
@@ -98,7 +98,7 @@ vektU2[i]= (vekt3[i]-vekt1[i]);
 }
 
 
-// Vektör sistemini de yeni vektör eklemek için 1'e basýn devam etmek için 0'a basýn gibi yap
+
 	}
 	}
 }
